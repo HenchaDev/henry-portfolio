4c72ed518986cefcbae3d5fa8e2c872c4739f33c
HenchaDev <henchaedits01@gmail.com>
2025-01-28 12:27:03 +0300

    working device sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,56 +2,66 @@
 module.exports = {
 	darkMode: ["class"],
 	content: [
-	  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-	  "./components/**/*.{js,ts,jsx,tsx,mdx}",
-	  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
-	  container: {
-		center: true,
-		padding: "15px",  // You can also add responsive padding if needed
-	  },
-	  screens: {
-		sm: '640px',
-		md: '768px',
-		lg: '960px',
-		xl: '1200px',
-	  },
-	  fontFamily: {
-		primary: "var(--font-jetbrainsMono)",
-	  },
-
-	  extend: {
-
-		colors: {
-			primary: "#1c1c22",
-			accent: {
-				DEFAULT: "#00ff99",
-				hover: "#00e187",
-			}
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: "1rem", // Default padding for all screens
+				sm: "1.5rem",    // Padding for sm screens and up
+				md: "2rem",      // Padding for md screens and up
+				lg: "2.5rem",    // Padding for lg screens and up
+				xl: "3rem",      // Padding for xl screens and up
+			},
+			screens: {
+				sm: "640px",
+				md: "768px",
+				lg: "1024px",
+				xl: "1280px",
+				"2xl": "1536px",
+			},
 		},
-
-		keyframes: {
-		  "accordion-down": {
-			from: { height: "0" },
-			to: { height: "var(--radix-accordion-content-height)" },
-		  },
-		  "accordion-up": {
-			from: { height: "var(--radix-accordion-content-height)" },
-			to: { height: "0" },
-		  },
+		screens: {
+			sm: "640px",
+			md: "768px",
+			lg: "1024px",
+			xl: "1280px",
+			"2xl": "1536px",
 		},
-		animation: {
-		  "accordion-down": "accordion-down 0.2s ease-out",
-		  "accordion-up": "accordion-up 0.2s ease-out",
+		fontFamily: {
+			primary: "var(--font-jetbrainsMono)",
 		},
-	  },
+		extend: {
+			colors: {
+				primary: "#1c1c22",
+				accent: {
+					DEFAULT: "#00ff99",
+					hover: "#00e187",
+				},
+			},
+			keyframes: {
+				"accordion-down": {
+					from: { height: "0" },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: "0" },
+				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+			},
+		},
 	},
 	plugins: [
-	  require("tailwindcss-animate"),
-	  // Additional plugins (if needed)
-	  // require('@tailwindcss/typography'),
-	  // require('@tailwindcss/forms'),
+		require("tailwindcss-animate"),
+		// Additional plugins (if needed)
+		// require('@tailwindcss/typography'),
+		// require('@tailwindcss/forms'),
 	],
-  };
-  
+};
